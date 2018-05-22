f3bb9a93c9ae6caa9f139c23d9f772b833558539
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-05-22 15:31:54 +1000

    Remove some create-elm-app cruft

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import './main.css';
 import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
 Main.embed(document.getElementById('root'));
-
-registerServiceWorker();
