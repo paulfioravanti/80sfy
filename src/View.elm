dac88863d6c715f93219ef2f87300a406d049c3a
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-06-03 17:05:34 +1000

    Remove unneeded let

--- src/View.elm
+++ src/View.elm
@@ -11,16 +11,9 @@ import VideoPlayer exposing (VideoPlayer)
 
 view : Model -> Html Msg
 view { audioPlayer, controlPanel, secretConfig, videoPlayer1, videoPlayer2 } =
-    let
-        visibleVideoPlayer =
-            if videoPlayer1.visible then
-                videoPlayer1
-            else
-                videoPlayer2
-    in
-        div [ attribute "data-name" "container" ]
-            [ ControlPanel.view audioPlayer controlPanel
-            , VideoPlayer.view videoPlayer1
-            , VideoPlayer.view videoPlayer2
-            , SecretConfig.view secretConfig
-            ]
+    div [ attribute "data-name" "container" ]
+        [ ControlPanel.view audioPlayer controlPanel
+        , VideoPlayer.view videoPlayer1
+        , VideoPlayer.view videoPlayer2
+        , SecretConfig.view secretConfig
+        ]
