3e63dd2c92a44573fda3b28a9a7eda467a0784c7
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-06-08 17:05:19 +1000

    Clean up VideoPlayer module

--- src/VideoPlayer.elm
+++ src/VideoPlayer.elm
@@ -3,17 +3,10 @@ module VideoPlayer
         ( Msg
         , VideoPlayer
         , init
-        , animateStyle
-        , newVisibility
-        , setSuccessGifUrl
         , subscriptions
-        , toggleFullScreen
         , toggleFullScreenMsg
-        , togglePlaying
         , togglePlayingMsg
-        , toggleVideoPlay
         , update
-        , updateVisibility
         , view
         )
 
@@ -26,8 +19,8 @@ import VideoPlayer.Model as Model
 import VideoPlayer.Msg as Msg
 import VideoPlayer.Ports as Ports
 import VideoPlayer.Subscriptions as Subscriptions
-import VideoPlayer.View as View
 import VideoPlayer.Update as Update
+import VideoPlayer.View as View
 
 
 type alias VideoPlayer =
@@ -43,32 +36,9 @@ init id visible zIndex =
     Model.init id visible zIndex
 
 
-animateStyle : Animation.Msg -> VideoPlayer -> VideoPlayer
-animateStyle msg videoPlayer =
-    { videoPlayer | style = Animation.update msg videoPlayer.style }
-
-
-newVisibility : VideoPlayer -> ( Bool, String )
-newVisibility videoPlayer1 =
-    if videoPlayer1.visible then
-        ( False, "1" )
-    else
-        ( True, "2" )
-
-
-setSuccessGifUrl : String -> VideoPlayer -> VideoPlayer
-setSuccessGifUrl gifUrl videoPlayer =
-    { videoPlayer | gifUrl = Success gifUrl }
-
-
 subscriptions : MsgConfig msg -> Bool -> VideoPlayer -> Sub msg
 subscriptions msgConfig fetchNextGif videoPlayer1 =
     Subscriptions.subscriptions msgConfig fetchNextGif videoPlayer1
-
-
-toggleFullScreen : Cmd msg
-toggleFullScreen =
-    Ports.toggleFullScreen ()
 
 
 toggleFullScreenMsg : Msg
@@ -76,19 +46,9 @@ toggleFullScreenMsg =
     Msg.ToggleFullScreen
 
 
-togglePlaying : Bool -> VideoPlayer -> VideoPlayer
-togglePlaying playing videoPlayer =
-    { videoPlayer | playing = playing }
-
-
 togglePlayingMsg : Bool -> Msg
 togglePlayingMsg =
     Msg.TogglePlaying
-
-
-toggleVideoPlay : Bool -> Cmd msg
-toggleVideoPlay play =
-    Ports.toggleVideoPlay play
 
 
 update :
@@ -99,25 +59,6 @@ update :
     -> ( VideoPlayer, VideoPlayer, Cmd msg )
 update msgConfig msg videoPlayer1 videoPlayer2 =
     Update.update msgConfig msg videoPlayer1 videoPlayer2
-
-
-updateVisibility : Bool -> VideoPlayer -> VideoPlayer
-updateVisibility visible videoPlayer =
-    let
-        newOpacity =
-            if videoPlayer.visible then
-                0
-            else
-                1
-
-        animateToNewOpacity =
-            Animation.interrupt
-                [ Animation.to
-                    [ Animation.opacity newOpacity ]
-                ]
-                videoPlayer.style
-    in
-        { videoPlayer | style = animateToNewOpacity, visible = visible }
 
 
 view : MsgConfig msg -> VideoPlayer -> Html msg
