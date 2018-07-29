b20c6cf2d44d3f937d4f706f095b023525e9616d
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-29 12:38:25 +1000

    Get first time load of the site working as I expect

--- src/audioPlayer.js
+++ src/audioPlayer.js
@@ -12,27 +12,40 @@ function initAudioPlayer(app) {
   app.ports.initAudioPlayer.subscribe((volume) => {
     window.requestAnimationFrame(() => {
       scPlayer = SC.Widget("track-player")
-      // NOTE: Events need to be placed under the READY event, even though
-      // doing so currently seems to halt the videoplayer when the app first
-      // loads. Moving the other event bindings outside of the READY scope
-      // does enable the video player to keep on playing, but the call to
-      // `getSounds` fails and hence the audio player only ever ends up with
-      // one track in it.
       scPlayer.bind(SC.Widget.Events.READY, () => {
         scPlayer.setVolume(volume)
         scPlayer.getSounds((sounds) => {
           app.ports.setPlaylistLength.send(sounds.length)
         })
-        scPlayer.bind(SC.Widget.Events.PLAY, () => {
-          app.ports.audioPlaying.send(null)
+        scPlayer.bind(SC.Widget.Events.PLAY, sound => {
+          // Only let Elm know about SoundCloud player play events if at least
+          // some of the sound has been loaded and can therefore actually play.
+          if (sound.loadedProgress > 0) {
+            app.ports.audioPlaying.send(null)
+          }
         })
-        scPlayer.bind(SC.Widget.Events.PAUSE, () => {
-          app.ports.audioPaused.send(null)
+        scPlayer.bind(SC.Widget.Events.PAUSE, sound => {
+          // Only let Elm know about SoundCloud player pause events if the
+          // sound has actually first been played.
+          if (sound.currentPosition > 0) {
+            app.ports.audioPaused.send(null)
+          }
         })
         scPlayer.bind(SC.Widget.Events.FINISH, () => {
           app.ports.requestNextTrackNumber.send(null)
         })
       })
+      // NOTE: This call is to make sure that when the site is first loaded,
+      // the videos play (without sound), but the control panel button shows
+      // the play button to start the SoundCloud player (and technically the
+      // videos as well), rather than the pause button.
+      //
+      // This is a bit of a hack for aesthetic reasons: I didn't want the site
+      // to start with videos playing for a bit, and then automatically paused
+      // suddenly for what looks like no reason. This is also the only time when
+      // the controls on the control panel and the video playback itself should
+      // be "out of sync".
+      app.ports.videosPlaying.send(null)
     })
   })
 }
