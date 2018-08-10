1fed6e38bc87c4011807c251a1ca8f7273e2df19
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-08-10 12:27:32 +1000

    Add link to StackOverflow question regarding full screen APIs with Firefox

--- src/Browser/Ports.elm
+++ src/Browser/Ports.elm
@@ -25,8 +25,9 @@ port mozFullScreenToggle : () -> Cmd msg
    you will see "Request for fullscreen was denied because
    Element.requestFullscreen() was not called from inside a short running
    user-generated event handler.". I don't currently know how to fix this.
-   More information can be found in this message on the Elm mailing list:
-   https://groups.google.com/d/msg/elm-dev/hhNu6SGOM54/TS0pDPtKCAAJ
+   More information can be found at:
+   - https://groups.google.com/d/msg/elm-dev/hhNu6SGOM54/TS0pDPtKCAAJ
+   - https://stackoverflow.com/q/43240352/567863
 -}
 port mozRequestFullScreen : () -> Cmd msg
 
