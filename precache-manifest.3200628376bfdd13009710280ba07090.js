ba99a0b02a617e59e40f9fdf09e0f7e69012a961
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-12-19 22:02:24 +1100

    Updates

---
+++ precache-manifest.3200628376bfdd13009710280ba07090.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c6e00cc84f646d8df2aee690e292f7",
+    "url": "/assets/giphy.png"
+  },
+  {
+    "revision": "26f83a627adf7062c00e65aaa7a52065",
+    "url": "/assets/logo.png"
+  },
+  {
+    "revision": "d9a46ffca7a04a71fa4b6ee5f95274bc",
+    "url": "/assets/scanline-overlay.png"
+  },
+  {
+    "revision": "4e22b4578faabb84559c9514a264e0cf",
+    "url": "/assets/soundcloud.png"
+  },
+  {
+    "revision": "5f897c934505322ad98b3d2c698b619f",
+    "url": "/assets/tv-static.mp4"
+  },
+  {
+    "revision": "64631526a65d6f19d1a1f1a2b4793992",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "fbc91a7b6a45fe05d8fc004384ac61d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68275753ee4c4514dce2006a4870b68f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "1c1ffcdf60ae37337bbf",
+    "url": "/static/css/vendors~main.8774fdaa.chunk.css"
+  },
+  {
+    "revision": "2e5d628a03c93618c509",
+    "url": "/static/js/main.2e5d628a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "1c1ffcdf60ae37337bbf",
+    "url": "/static/js/vendors~main.1c1ffcdf.chunk.js"
+  },
+  {
+    "revision": "140f41485edce6f713abe17625eba4c1",
+    "url": "/static/media/fa-solid-900.140f4148.woff"
+  },
+  {
+    "revision": "4910ec733558f59bf05834d4f831a48d",
+    "url": "/static/media/fa-solid-900.4910ec73.eot"
+  },
+  {
+    "revision": "813b8aee60f235b36887a388b70e1359",
+    "url": "/static/media/fa-solid-900.813b8aee.woff2"
+  },
+  {
+    "revision": "e57e108a1ae04ca2b27cab75e4478867",
+    "url": "/static/media/fa-solid-900.e57e108a.ttf"
+  },
+  {
+    "revision": "e98a92ac980c63c46a7e25c4fbcf30ef",
+    "url": "/static/media/fa-solid-900.e98a92ac.svg"
+  },
+  {
+    "revision": "459cd7b15f462550fd71eec22f51e196",
+    "url": "/tags.json"
+  }
+]);
